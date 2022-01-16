1ebeb1309dc2941c92d1f556492e254ac7608cf8
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-16 20:36:59 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle(10, 20);
+            Console.WriteLine($"perimeter = {rect.Perimeter}");
+            Console.WriteLine($"area = {rect.Area}");
+        }
+    }
+}
---
+++ Task2/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Rectangle
+    {
+
+        private double side1;
+        public double Side1
+        {
+            get { return side1; }
+        }
+
+        private double side2;
+        public double Side2
+        {
+            get { return side2; }
+        }
+
+        public double Area
+        {
+            get { return areaCalculator(); }
+        }
+        public double Perimeter
+        {
+            get { return perimeterCalculator(); }
+        }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        private double areaCalculator()
+        {
+            return side1 * side2;
+        }
+        private double perimeterCalculator()
+        {
+            return side2 * 2 + side1 * 2;
+        }
+    }
+}
