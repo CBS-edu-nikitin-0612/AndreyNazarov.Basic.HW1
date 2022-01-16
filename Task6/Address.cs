cd0c3a5761189785a44d70e98e6a9b1ff15b6c77
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-16 22:57:55 +0300

    feat: Task6

---
+++ Task6/Address.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Address
+    {
+        private uint index;
+
+        public uint Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        private string country;
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        private string city;
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        private string street;
+
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+
+        private string house;
+
+        public string House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+
+        private string appartment;
+
+        public string Appartment
+        {
+            get { return appartment; }
+            set { appartment = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{index}, {country}, {city}, {street}, house {house}, app. {appartment}");
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = new Address()
+            {
+                Index = 123456,
+                Country = "USA",
+                City = "New York",
+                Street = "Broadway",
+                House = "45a",
+                Appartment = "23d"
+            };
+            address.Print();
+        }
+    }
+}
