03b0b4277665809c784cba534f26fad79871786b
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-16 21:00:11 +0300

    feat: Added possibility to use different values by user

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,9 +6,20 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            var rect = new Rectangle(10, 20);
-            Console.WriteLine($"perimeter = {rect.Perimeter}");
-            Console.WriteLine($"area = {rect.Area}");
+            Console.WriteLine("Please type sides of rectangle to receiving area and  perimeter (use space as separator)");
+            var str = Console.ReadLine().Split(' ');
+            double side1;
+            double side2;
+            if (double.TryParse(str[0], out side1) && double.TryParse(str[1], out side2))
+            {
+                var rect = new Rectangle(side1, side2);
+                Console.WriteLine($"perimeter = {rect.Perimeter}");
+                Console.WriteLine($"area = {rect.Area}");
+            }
+            else
+            {
+                Console.WriteLine("Incorect data");
+            }
         }
     }
 }
